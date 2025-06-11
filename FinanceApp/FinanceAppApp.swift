15f91840a9f3d8ad245d169408243412d13459c5
tasyaglk <galkina.tasya@mail.ru>
2025-06-11 15:40:51 +0300

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Тася Галкина on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
