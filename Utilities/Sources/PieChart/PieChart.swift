427711d28d958f5ad18472fc75ff8630022a1ec5
tasyaglk <galkina.tasya@mail.ru>
2025-07-25 03:29:22 +0300

    add Utilities spm

---
+++ Utilities/Sources/PieChart/PieChart.swift
@@ -0,0 +1,14 @@
+//
+//  PieChart.swift
+//  Utilities
+//
+//  Created by Тася Галкина on 25.07.2025.
+//
+
+import Foundation
+
+public struct PieChart {
+    public static func hello() {
+        print("Hello from PieChart!")
+    }
+}
